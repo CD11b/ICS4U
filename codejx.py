24b294f265166b5c679e865f31cd37625cdea154
CanadianFaker <47955316+CanadianFaker@users.noreply.github.com>
2019-03-25 12:49:07 -0400

    Update codejx.py

--- codejx.py
+++ codejx.py
@@ -52,15 +52,104 @@ class Player:
             win.blit(self.lookIdle[0], (self.x, self.y))
 
 
-guard = Player(125, 50, 40, 60, 'guard')
+player = Player(125, 50, 40, 60, 'player')
+
+
+class Guard:
+    def __init__(self, x, y, width, height, name):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 2
+        self.left = False
+        self.right = False
+        self.down = False
+        self.up = False
+
+        self.lookUp = [pygame.image.load(self.name + '0.jpg')]
+        self.lookDown = [pygame.image.load(self.name + '180.jpg')]
+        self.lookLeft = [pygame.image.load(self.name + '270.jpg')]
+        self.lookRight = [pygame.image.load(self.name + '90.jpg')]
+        self.lookIdle = [pygame.image.load(self.name + '180.jpg')]
+
+    def draw(self):
+        if self.left:
+            win.blit(self.lookLeft[0], (self.x, self.y))
+
+        elif self.right:
+            win.blit(self.lookRight[0], (self.x, self.y))
+
+        elif self.down:
+            win.blit(self.lookDown[0], (self.x, self.y))
+
+        elif self.up:
+            win.blit(self.lookUp[0], (self.x, self.y))
+
+        else:
+            win.blit(self.lookIdle[0], (self.x, self.y))
+
+
+def GuardLeft():
+    if keys[pygame.K_LEFT] and player.x > 0 + 3:
+        if not player.left and player.right and player.up and player.down:
+            Guard.x -= Guard.vel
+            Guard.left = True
+            Guard.right = False
+            Guard.down = False
+            Guard.up = False
+        else:
+            pass
+
+
+def GuardRight():
+    if keys[pygame.K_RIGHT] and player.x < screen_width - player.height - 25:
+        if not player.left and player.right and player.up and player.down:
+            Guard.x -= Guard.vel
+            Guard.left = False
+            Guard.right = True
+            Guard.down = False
+            Guard.up = False
+        else:
+            pass
+
+
+def GuardUp():
+    if keys[pygame.K_UP] and player.y > 0:
+        if not player.left and player.right and player.up and player.down:
+            Guard.y += Guard.vel
+            Guard.left = False
+            Guard.right = False
+            Guard.down = False
+            Guard.up = True
+        else:
+            pass
+
+
+def GuardDown():
+    if keys[pygame.K_DOWN] and player.y < 0 + 365:
+        if not player.left and player.right and player.up and player.down:
+            Guard.y -= Guard.vel
+            Guard.left = False
+            Guard.right = False
+            Guard.down = True
+            Guard.up = False
+        else:
+            pass
+
+
+Guard = Guard(200, 200, 40, 60, 'Guard')
+
 
 clock = pygame.time.Clock()
 run = True
 
 
 def redrawGameWindow():
-    win.blit(bg,(0,0))
-    guard.draw()
+    win.blit(bg, (0, 0))
+    player.draw()
+    Guard.draw()
     pygame.display.update()
 
 
@@ -72,33 +161,57 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and guard.x > 0 + 3:
-        guard.x -= guard.vel
-        guard.left = True
-        guard.right = False
-        guard.down = False
-        guard.up = False
+    if keys[pygame.K_LEFT] and player.x > 0 + 3:
+        player.x -= player.vel
+        player.left = True
+        player.right = False
+        player.down = False
+        player.up = False
+        Guard.x -= Guard.vel
+        Guard.left = True
+        Guard.right = False
+        Guard.down = False
+        Guard.up = False
+        GuardLeft()
 
-    if keys[pygame.K_RIGHT] and guard.x < screen_width - guard.height - 25 :
-        guard.x += guard.vel
-        guard.left = False
-        guard.right = True
-        guard.down = False
-        guard.up = False
+    if keys[pygame.K_RIGHT] and player.x < screen_width - player.height - 25:
+        player.x += player.vel
+        player.left = False
+        player.right = True
+        player.down = False
+        player.up = False
+        Guard.x += Guard.vel
+        Guard.left = False
+        Guard.right = True
+        Guard.down = False
+        Guard.up = False
+        GuardRight()
 
-    if keys[pygame.K_UP] and guard.y > 0:
-        guard.y -= guard.vel
-        guard.left = False
-        guard.right = False
-        guard.down = False
-        guard.up = True
+    if keys[pygame.K_UP] and player.y > 0:
+        player.y -= player.vel
+        player.left = False
+        player.right = False
+        player.down = False
+        player.up = True
+        Guard.y -= Guard.vel
+        Guard.left = False
+        Guard.right = False
+        Guard.down = False
+        Guard.up = True
+        GuardUp()
 
-    if keys[pygame.K_DOWN] and guard.y < 0 + 365:
-        guard.y += guard.vel
-        guard.left = False
-        guard.right = False
-        guard.down = True
-        guard.up = False
+    if keys[pygame.K_DOWN] and player.y < 0 + 365:
+        player.y += player.vel
+        player.left = False
+        player.right = False
+        player.down = True
+        player.up = False
+        Guard.y += Guard.vel
+        Guard.left = False
+        Guard.right = False
+        Guard.down = True
+        Guard.up = False
+        GuardDown()
 
     redrawGameWindow()
     clock.tick(60)
