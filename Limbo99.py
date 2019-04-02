e1164db57b516185e4b32f6a540e0bd55c3efae8
GregMpour <38716745+GregMpour@users.noreply.github.com>
2019-04-01 22:14:52 -0400

    Update Limbo99.py

--- Limbo99.py
+++ Limbo99.py
@@ -89,7 +89,7 @@ class Click:
 clock = pygame.time.Clock()
 run = True
 
-guard = Mob(100, 100, 82, 49, 'guard', 6)
+guard = Mob(100, 100, 82, 49, 'GuardGun', 6)
 player = Player(200, 200, 40, 60, 'prisoner', 6)
 b = Bullet(guard.x, guard.y, 6, black, 0)
 bullets = []
@@ -149,40 +149,40 @@ while run:
 
                         if guard.left:
                             if clicks[s][0] <= guard.x:
-                                bullets.append(Bullet(guard.x, guard.y + 82, 6, black, 0))
-                                bullet_velocity(0, 82)
+                                bullets.append(Bullet(guard.x, guard.y + 75, 4, black, 0))
+                                bullet_velocity(0, 75)
                                 shots.append([])
                                 break
                             break
 
                         if guard.right:
                             if clicks[s][0] >= guard.x + 42:
-                                bullets.append(Bullet(guard.x + 42, guard.y + 82, 6, black, 0))
-                                bullet_velocity(42, 82)
+                                bullets.append(Bullet(guard.x + 57, guard.y + 7, 4, black, 0))
+                                bullet_velocity(57, 7)
                                 shots.append([])
                                 break
                             break
 
                         if guard.up:
                             if clicks[s][1] <= guard.y:
-                                bullets.append(Bullet(guard.x, guard.y, 6, black, 0))
-                                bullet_velocity(0, 0)
+                                bullets.append(Bullet(guard.x + 7, guard.y, 4, black, 0))
+                                bullet_velocity(7, 0)
                                 shots.append([])
                                 break
                             break
 
                         if guard.down:
                             if clicks[s][1] >= guard.y + 42:
-                                bullets.append(Bullet(guard.x, guard.y + 42, 6, black, 0))
-                                bullet_velocity(0, 42)
+                                bullets.append(Bullet(guard.x + 75, guard.y + 57, 4, black, 0))
+                                bullet_velocity(75, 57)
                                 shots.append([])
                                 break
                             break
 
                         if guard.lookIdle:
                             if clicks[s][1] <= guard.y:
-                                bullets.append(Bullet(guard.x, guard.y, 6, black, 0))
-                                bullet_velocity(0, 0)
+                                bullets.append(Bullet(guard.x + 7, guard.y, 4, black, 0))
+                                bullet_velocity(7, 0)
                                 shots.append([])
                                 break
                             break
@@ -197,37 +197,37 @@ while run:
                 bullets[n].y += bullets[n].velocity[1]
                 n += 1
 
-            if screen_width < bullet.x < 0:
+            if screen_width < bullet.x or bullet.x < 0:
                 bullets.pop(bullets.index(bullet))
 
-            if screen_height < bullet.y > 0:
+            if screen_height < bullet.y or bullet.y < 0:
                 bullets.pop(bullets.index(bullet))
 
         else:
             bullets.pop(bullets.index(bullet))
 
-    if keys[pygame.K_LEFT] and guard.x > 0:
+    if keys[pygame.K_a] and guard.x > 0:
         guard.x -= guard.speed
         guard.left = True
         guard.right = False
         guard.down = False
         guard.up = False
 
-    if keys[pygame.K_RIGHT] and guard.x + guard.width < screen_width - guard.width:
+    if keys[pygame.K_d] and guard.x + guard.width < screen_width - guard.width:
         guard.x += guard.speed
         guard.left = False
         guard.right = True
         guard.down = False
         guard.up = False
 
-    if keys[pygame.K_UP] and guard.y > 0:
+    if keys[pygame.K_w] and guard.y > 0:
         guard.y -= guard.speed
         guard.left = False
         guard.right = False
         guard.down = False
         guard.up = True
 
-    if keys[pygame.K_DOWN] and guard.y + guard.width < screen_height - guard.height:
+    if keys[pygame.K_s] and guard.y + guard.width < screen_height - guard.height:
         guard.y += guard.speed
         guard.left = False
         guard.right = False
